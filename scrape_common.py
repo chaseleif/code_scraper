7cf5a3e2b49191cbe1362a4f67e61b1f32c16227
chaseleif <chaseleif@icloud.com>
2023-10-14 22:23:55 -0500

    Add license message to source files

--- scrape_common.py
+++ scrape_common.py
@@ -6,6 +6,24 @@ from re import sub
 from sys import stdout, stderr
 from time import sleep, time
 
+'''
+    scrape_common.py
+    Copyright (C) 2023  Chase Phelps
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 class Scraped:
   done = 0
   attempts = 0
